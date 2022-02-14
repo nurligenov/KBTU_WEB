8794da79ac00631b05e3dba3471e1dc39d575399
nurligenov <66307255+nurligenov@users.noreply.github.com>
2022-02-14 23:46:48 +0600

    Add files via upload

---
+++ solutions/sol2.js
@@ -0,0 +1,13 @@
+let admin, name;
+name = "John";
+admin = name;
+alert( admin );
+
+
+
+let thirdPlanetName = "Earth";
+let currentUserName = "Tim";
+
+// const BIRTHDAY = '18.04.1982';
+
+// const age = someCode(BIRTHDAY);
